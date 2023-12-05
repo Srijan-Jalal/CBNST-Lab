828c3476b42ea127b70298065be80f8fe167016c
Srijan <jalalsrijan@gmail.com>
2023-12-05 08:32:28 +0530

    Exp-16 of CBNST Lab on 28/11/23

--- EXP-16.c
+++ EXP-16.c
@@ -3,17 +3,17 @@
 
 int main(void) 
 {
-    int n,i,k;
+    int n,i,k,it=0;
 
-    printf("Enter the Total Number of Equations: ");
-    scanf("%d", &n);
+    printf("Enter the Total Number of Unknowns: ");
+    scanf("%d",&n);
 
     float AdjM[n][n+1], y[n],e,er,ce,temp;
 
     printf("Enter The Pre-Specified Error: ");
-    scanf("%f", &e);
+    scanf("%f",&e);
 
-    printf("Enter the Argumented Matrix: \n");
+    printf("Enter the Argumented Matrix:\n");
     for(i = 1; i <= n; i++) 
     {
         for(k = 1; k <= n + 1; k++) 
@@ -26,10 +26,18 @@ int main(void)
     {
         y[i] = 0;
     }
-    
+
+    printf("\nITERATION \t X \t Y \t Z \n");
+    printf("%d \t\t",it);
+    for(i = 1; i <= n; i++) 
+    {
+        printf("%.4f \t",y[i]);
+    }
+    printf("\n");
     do 
     {
         er = 0;
+        printf("%d \t\t",++it);
         for(i = 1; i <= n; i++) 
         {
             float sum = 0;
@@ -45,63 +53,15 @@ int main(void)
                 er = ce;
             }
             y[i] = temp;
-            printf("Y[%d] = %f\n", i, y[i]);
+            printf("%.4f\t",y[i]);
         }
+        printf("\n");
     } while(er >= e);
-    
-    printf("\nSolution\n");
-    for(i = 1; i <= n; i++) {
-        printf("Y[%d]: %f\n", i, y[i]);
+    printf("\nSolution: \t");
+    for(i = 1; i <= n; i++) 
+    {
+        printf("%.4f \t",y[i]);
     }
-    
+    printf("\n");
     return 0;
 }
-
-
-
-// #include <stdio.h>
-// #include <math.h>
-// #include <stdbool.h>
-
-// float findx(float y,float z);
-// float findy(float z,float x);
-// float findz(float x,float y);
-
-// int main(void)
-// {
-//     float x=0,y=0,z=0;
-//     int i=0,N;
-//     printf("Given Equations are: \n");
-//     printf("3x + 1y + 1z = 8 \n");
-//     printf("2x + 3y + 1z = 11 \n");
-//     printf("1x + 2y + 3z = 14 \n");
-//     printf("Enter The Number Of Interations: ");
-//     scanf("%d",&N);
-//     printf("Iteration \tX \t\t Y \t\t Z \n");
-//     do{
-//         printf("%d \t\t%.4f \t\t %.4f \t %.4f \n",i,x,y,z);
-
-//         x = findx(y,z);
-//         y = findy(z,x);
-//         z = findz(x,y);
-//         i++;
-
-//     }while(i<=N);
-//     printf("Solution is: \t%.4f \t\t %.4f \t %.4f \n",x,y,z);
-//     return 0;
-// }
-
-// float findx(float y,float z)
-// {
-//     return (8-y-z)/3;
-// }
-
-// float findy(float z,float x)
-// {
-//     return (11-z-(2*x))/3;
-// }
-
-// float findz(float x,float y)
-// {
-//     return (14-(2*y)-x)/3;
-// }
